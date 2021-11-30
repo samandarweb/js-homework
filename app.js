d9bc2f965771550ff669c5afb8467894d83761c3
samandarweb <samandarweb@gmail.com>
2021-11-30 15:51:23 +0300

    add finished

---
+++ app.js
@@ -0,0 +1,32 @@
+// Vazifa : 1-masala
+
+/*for (let i = 0; i < 10; i++) {
+    console.log(`Samandar`); 
+}*/
+
+// Vzifa : 2-masala
+
+/*const myArray = [ [false, true], [1, 2], 99, `yaxshi`]
+
+const newArray = []
+
+let i = 0
+
+while (i < myArray.length) {
+    if (typeof myArray[i])  {
+        newArray.push(myArray[i])
+    }
+    i++ 
+}
+console.log(newArray);*/
+
+//  Vazifa : 3-masala
+
+const data = [ [1, 2, 3], "qizil", [4, 5, 6],  true, false]
+
+let i = 0
+while (i < data.length) {
+    if (data.isArray())) {
+        
+    }
+}
