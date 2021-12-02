0d75eedbbbb1c7653598f00373496e4b2c930165
samandarweb <samandarweb@gmail.com>
2021-12-02 22:24:47 +0300

    addd

--- app.js
+++ app.js
@@ -19,16 +19,61 @@ while (i < myArray.length) {
    i++
 }*/
 
-
 //  Vazifa : 3-masala
 
-const data = [ [1, 2, 3], "qizil", [4, 5, 6],  true, false]
+/*const data = [ [1, 2, 3], "qizil", [4, 5, 6],  true, false]
 const newArray = []
 let i = 0
 while (i < data.length) {
-    if (typeof data[i] == "object") {
-        newArray.push(data[i])
+    if (Array.isArray()) {
+       
     }
     i++
+}*/
+// console.log(newArray);
+
+//    FOR LOOP VAZIFALARI
+// 5-masala  Berilgan qiymat: 'samarqand'
+// Kutilyotgan qiymat : 'dnaqramas' => elementlarni teskari qilib beradigan cod yozing.
+
+/*let shaharNomi = "samarqand";
+let shahar = shaharNomi.split('').reverse()
+let newShahar = []
+for (let i = 0; i < shahar.length; i++) {
+    newShahar = newShahar + shahar[i]
 }
-console.log(newArray);
+console.log(newShahar);*/
+
+// 6 -masala  Propmpdan kiritilgan istalgan so’zlarni, misol uchun “AsSSalamu AlAyKUM YaxShimisiz” so’zi kiritilgan bo’lsa. Uni:
+// “aSSALOMU aLAYKUM yAXSHIMISIZ” ko’rinishida qaytaring.
+/*const work = prompt('soz kiriting').split()
+
+let words = ' '
+
+
+for (let i = 0; i < working.length; i++) {
+    words = work[i].charAt().toLowerCase + working[i].slice(1).toUpperCase 
+    
+}
+console.log(words);*/
+// 7-masala  Arraydagi Natural raqamlarni yig’indisini topadigan cod yozing.
+
+// 8-masala Prompt’dan kiritilgan stringlar ichidagi “a” harfi necha marta ishtirok etganini aniqlaydigan cod yozing.
+// “Qalesiz yaxshimisiz ? Yaxshimi ishlar ?” - nechta “a” harifi qatnashganini toping.
+ /*const matn = prompt (`soz:`)
+
+let qisqaMatn = []
+
+for(let i = 0; i < matn.length;   i++) {
+
+    if(matn[i].includes("a")) {
+        qisqaMatn.push(matn[i])
+    }
+}
+alert(qisqaMatn.length)*/
+
+// 9-masala  [21.1, "Roziya", "array", ["I am array"], null, true, 214] array berilgan
+// Uni:
+// ["number", "string", "string", "object", "object", "boolean", "number"] ko’rinishida qaytaring.
+
+// 10-masala Kiritilgan textni nechta elementdan iboratligini aniqlovchi cod yozing.lendth dan FOIDALANMANG.
